ba283f214cd0c4e0b5b8b5a3f7e054234703514a
Neowutran <me@neowutran.ovh>
2017-07-19 23:41:27 +0200

    simplify

--- ActivateGPUS1UI/ByteArrayRocks.cs
+++ ActivateGPUS1UI/ByteArrayRocks.cs
@@ -1,32 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
-using System.Text.RegularExpressions;
 
 static class ByteArrayRocks
 {
-
     public static byte[] StringToByteArray(this string hex)
     {
         return Enumerable.Range(0, hex.Length)
                          .Where(x => x % 2 == 0)
                          .Select(x => Convert.ToByte(hex.Substring(x, 2), 16))
                          .ToArray();
-    }
-
-
-    public static string ReplaceAll(this string src, string search, string repl)
-    {
-        Debug.WriteLine("replace start for:" + search);
-        return src.Replace(search.Replace(" ", ""), repl.Replace(" ", ""));
-    }
-
-    public static string ReplaceAll(this string src, Regex search, string repl)
-    {
-        var match = search.Match(src);
-        Debug.WriteLine(match.Value);
-        return search.Replace(src, repl.Replace(" ", ""));
-
     }
 }
--- ActivateGPUS1UI/Program.cs
+++ ActivateGPUS1UI/Program.cs
@@ -1,60 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Globalization;
 using System.IO;
-using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace ActivateGPUS1UI
 {
   class Program
     {
+        private static readonly byte FLAG_GPU = 2;
+        private static readonly byte FLAG_DIRECT_BLIT = 4;
+        private static readonly String PATH = @"F:\Tera\S1UI\";
 
-        private static readonly String _path = @"F:\Tera\S1UI\";
         static void Main(string[] args)
         {
-            var countPattern = 0;
-            var countFiles = 0;
-            var filesWithoutMatchs = new List<String>();
-            var allFiles = Directory.GetFiles(_path, "*.gpk", SearchOption.AllDirectories);
-            foreach (String file in allFiles)
+            foreach (var file in Directory.GetFiles(PATH, "*.gpk", SearchOption.AllDirectories))
             {
-                countFiles++;
-                var baseName = BitConverter.ToString(Encoding.ASCII.GetBytes(file.Replace(".gpk", "").Replace("S1UI_", ""))).Replace("-", "");
-                string data = BitConverter.ToString(File.ReadAllBytes(file)).Replace("-", "");
-                //var GPUPattern = new Regex("474758.{"+30*2+"}"+baseName+".*2E7467614411)");
-                //var GPUPattern = "2E7467614411(.)";*
-                var GPUPattern = "474658.{30,400}4411(?<key>.).{7}";
-
-                var listMatch = Regex.Matches(data, GPUPattern);
-                if (listMatch.Count == 0) {
-                    filesWithoutMatchs.Add(file);
-                }
-                foreach ( Match match in listMatch)
+                var data = BitConverter.ToString(File.ReadAllBytes(file)).Replace("-", "");
+                foreach ( Match match in Regex.Matches(data, "474658.{30,400}4411(?<key>.).{7}"))
                 {
-                    Byte gpuParameters = Convert.ToByte(match.Groups[1].Value);
-                    gpuParameters |= 4;
-                    gpuParameters |= 2;
-                    Debug.WriteLine(gpuParameters.ToString("X1"));
+                    var gpuParameters = Convert.ToByte(match.Groups[1].Value);
+                    gpuParameters |= FLAG_DIRECT_BLIT;
+                    gpuParameters |= FLAG_GPU;
                     data = data.Remove(match.Groups[1].Index, 1).Insert(match.Groups[1].Index, gpuParameters.ToString("X1"));
-                    Debug.WriteLine(match.Groups[1].Index);
-                   // data.
-                   // Debug.WriteLine(match.Groups[0].Value);
-                    countPattern++;
                 }
-
                 File.WriteAllBytes(file, data.StringToByteArray());
-
-
             }
-
-            Debug.WriteLine("total count: files = "+ countFiles+ " ; pattern = " + countPattern);
-            Debug.WriteLine(String.Join(", ", filesWithoutMatchs.ToArray()));
-
-
         }
     }
 }
