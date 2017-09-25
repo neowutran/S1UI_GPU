034b87ce8ae1e51003c4791ee5aab556fbfb7190
SaltyMonkey <1570693+SaltyMonkey@users.noreply.github.com>
2017-09-25 14:53:59 +0300

    updated version

---
+++ ActivateGPUS1UI/ConsoleOutput.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActivateGPUS1UI
+{
+    internal static class ConsoleOutput
+    {
+        public static void ErrorMessage(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            JustMessage(message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        public static void InformationMessage(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            JustMessage(message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        public static void StandartMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+        private static void JustMessage(string message)
+        {
+            Console.WriteLine(String.Format("[{0}]:{1}", DateTime.Now, message));
+        }
+    }
+}
